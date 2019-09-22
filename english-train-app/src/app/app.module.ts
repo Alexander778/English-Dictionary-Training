6b84ddfd6af5d3e1941bbaf3fb6b1283a9e2fd65
Alexander Kuzmenko <oleksandr.kuzmenko@unity-bars.com>
2019-09-22 23:22:40 +0300

    - create login page - create empty main page

--- english-train-app/src/app/app.module.ts
+++ english-train-app/src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import {MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatCardModule,
+  MatIconModule,
+  MatTableModule} from '@angular/material';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+
+const routes =[
+  {path:'login',component:'loginComponent'},
+  {path:'main',component:'mainpageComponent'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
