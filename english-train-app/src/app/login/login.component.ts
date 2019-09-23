f20faf5ff57828195911e2fa92e292802355b820
Alexander Kuzmenko <oleksandr.kuzmenko@unity-bars.com>
2019-09-23 17:05:18 +0300

    FadeIn Animation Login Form
    - add aninamtion

--- english-train-app/src/app/login/login.component.ts
+++ english-train-app/src/app/login/login.component.ts
@@ -3,17 +3,56 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { DialogWindowComponent } from '../dialog-window/dialog-window.component';
+import { trigger, style, state, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('icoLState', [
+      state('shown', style({
+        'opacity': '1',
+        transform: 'translate(0)'
+      })),
+      state('hidden', style({
+        'opacity': '0',
+        transform: 'translate(155px)'
+      })),
+      transition('hidden => shown', animate('1.5s'))
+    ]),
+    trigger('icoRState', [
+      state('shown', style({
+        'opacity': '1',
+        transform: 'translate(0)'
+      })),
+      state('hidden', style({
+        'opacity': '0',
+        transform: 'translate(-155px)'
+      })),
+      transition('hidden => shown', animate('1.5s'))
+    ]),
+    trigger('mainPanel', [
+      state('shown', style({
+        'opacity': '1',
+        transform: 'translateY(0)'
+      })),
+      state('hidden', style({
+        'opacity': '0',
+        transform: 'translateY(-155px)'
+      })),
+      transition('hidden => shown', animate('1.5s'))
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
-
+  state = 'hidden';
   constructor(private router: Router) { }
 
   ngOnInit() {
+    if (this.state === 'hidden') {
+      setTimeout(() => this.state = 'shown');
+    }
   }
 
   onSubmit(form: NgForm) {
