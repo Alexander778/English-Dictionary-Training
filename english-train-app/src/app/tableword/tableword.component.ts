b742fa47a6211146cf54adace1dc4f53180fd073
Alexander Kuzmenko <oleksandr.kuzmenko@unity-bars.com>
2019-09-30 22:23:25 +0300

    - paging in tableword

--- english-train-app/src/app/tableword/tableword.component.ts
+++ english-train-app/src/app/tableword/tableword.component.ts
@@ -8,6 +8,7 @@ import {FillTableService} from '../services/fillTable.service';
 import {MatDialog, MatSnackBar} from '@angular/material';
 import {DialogWindowComponent} from '../dialog-window/dialog-window.component';
 import {DialogConfirmDelWordComponent} from '../dialog-confirm-del-word/dialog-confirm-del-word.component';
+import {isUndefined} from "util";
 
 @Component({
   selector: 'app-tableword',
@@ -16,20 +17,31 @@ import {DialogConfirmDelWordComponent} from '../dialog-confirm-del-word/dialog-c
 })
 export class TablewordComponent implements OnInit {
 
-  displayedColumns: string[] = ['Position', 'Word', 'Translation', 'Action'];
+  displayedColumns: string[] = ['Word', 'Translation', 'Action'];
   iconsState = {};
   words: Observable<Word[]>;
+  length;
+  pageSize = 10;  // set default to 10
+  pageSizeOptions = [5, 10, 25, 100];
+  pageIndex = 5;
+  wordPageCollection: AngularFirestoreCollection<Word>;
+  db: AngularFirestore;
+
+
 
 
   constructor(
     public wordService: FillTableService,
     private dialog: MatDialog,
-    public authService: AuthService) {
+    public authService: AuthService,
+    public _db: AngularFirestore) {
+
+    this.db = _db;
   }
 
   ngOnInit() {
     this.wordService.initTable(this.authService.user.email);
-    this.words = this.wordService.words;
+    this.words = this.getPagingWord();
   }
 
   openDialog(dialogCom: any, width: string, height: string): void {
@@ -58,6 +70,34 @@ export class TablewordComponent implements OnInit {
   deleteWord(element: any) {
     this.wordService.currentWord = element;
     this.openDialog(DialogConfirmDelWordComponent, '300px', '150px');
+
+  }
+
+  getPagingWord(event?: any): Observable<Word[]> {
+    if (!isUndefined(event)) {
+      this.wordPageCollection = this.db.collection<Word>('words',
+        ref => ref
+          .where('userId', '==', this.authService.user.email)
+          .orderBy('word')
+          .startAt(event.pageIndex)
+          .limit(event.pageSize));
+    } else {
+      this.wordPageCollection = this.db.collection<Word>('words',
+        ref => ref
+          .where('userId', '==', this.authService.user.email)
+          .orderBy('word')
+          .startAt(this.pageIndex)
+          .limit(this.pageSize));
+    }
+
+
+    /*this.wordPageCollection.get().then(snap => {
+      debugger;
+      this.length = snap.size;
+    });*/
+
+    this.words = this.wordPageCollection.valueChanges();
+    return this.words;
 
   }
 
